ec942729929f4f455c346e23e561fb54e5647df2
onsjannet <onsjannet@gmail.com>
2021-07-19 16:13:44 +0100

    Basic annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import math
+
+'''takes a float n as argument
+and returns the floor of the float.'''
+
+
+def floor (n: float) -> int:
+    ''' Return floor of n. '''
+    return math.floor(n)
