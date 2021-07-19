1188efaf540cfb26858f125de0b0ea0a509b4a2d
onsjannet <onsjannet@gmail.com>
2021-07-19 16:29:27 +0100

    Basic annotations

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-'''takes a float n as argument
-and returns the floor of the float.'''
+'''
+takes a float n as argument
+and returns the floor of the float as an int
+'''
+
 
 import math
 
+
 def floor(n: float) -> int:
-    ''' Return floor of n.
-    '''
+    ''' Return floor of n.'''
     return math.floor(n)
