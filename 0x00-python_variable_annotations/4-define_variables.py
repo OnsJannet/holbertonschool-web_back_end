f375fb9432d293b176f6f7c409d9f134121d45ef
onsjannet <onsjannet@gmail.com>
2021-07-19 16:43:16 +0100

    Define variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -5,7 +5,7 @@ annotated variables with the specified values:
 '''
 
 
-a : int = 1
-pi : float = 3.14
-i_understand_annotations : bool = True
-school : str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
