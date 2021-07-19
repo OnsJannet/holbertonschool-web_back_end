32ef909b27099d336008c45355058535de186d45
onsjannet <onsjannet@gmail.com>
2021-07-19 16:42:07 +0100

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+annotated variables with the specified values:
+
+'''
+
+
+a : int = 1
+pi : float = 3.14
+i_understand_annotations : bool = True
+school : str = "Holberton"
