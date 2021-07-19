3cb21e56e3ad0f16ac19629c8ed80780689c1e41
onsjannet <onsjannet@gmail.com>
2021-07-19 16:33:00 +0100

    Basic annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+takes a float n as argument and returns the string
+representation of the float as an str rep
+
+'''
+
+
+def to_str(n: float) -> str:
+    ''' Return string rep of n'''
+    return str(n)
