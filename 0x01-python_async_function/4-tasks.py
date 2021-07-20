292dd305c57d57f83834f0809b0d40c85466e4e3
onsjannet <onsjannet@gmail.com>
2021-07-20 14:20:52 +0100

    tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,14 +3,14 @@
 that waits for a random delay between 0 and 10
 seconds and eventually returns it.
 '''
-import asyncio
 import random
+import asyncio
 from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def task_wait_n(n, max_delay: int = 10) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     ''' Prints a list of delays in ascending order. '''
-    tasks = [asyncio.create_task(wait_random(max_delay))
-             for integer in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    task = [await wait_random(max_delay) for i in range(n)]
+    return sorted(task)
