bcb68f57204f2982590bad4cdf85d9cb92a5a27e
onsjannet <onsjannet@gmail.com>
2021-07-21 12:20:26 +0100

    Run time for four parallel comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,14 +5,15 @@ async_generator, then return the 10 random numbers
 '''
 import random
 import asyncio
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
-    ''' Collect 10 numbers and return them
-        
-     '''
+async def async_comprehension() -> List[float]:
+    '''
+        Collect 10 numbers and return them
+
+    '''
     numbers = []
     async for number in async_generator():
         numbers.append(number)
