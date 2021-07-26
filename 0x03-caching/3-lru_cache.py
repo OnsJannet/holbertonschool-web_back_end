51d40f6999c5c4cc2cb40bd5b18e48cfac8b0819
onsjannet <onsjannet@gmail.com>
2021-07-26 17:14:52 +0100

    LRU CACHING

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" LIFOCache module
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ BasicCache defines:
+      - Inherits from BaseCaching
+      - Cashing System with no limit
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ BaseCaching """
+        self.cache = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Assign to the dictionary """
+        if key and item:
+            if key in self.cache_data:
+                self.cache.remove(key)
+            self.cache_data[key] = item
+            self.cache.append(key)
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard = self.cache.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD:', discard)
+        else:
+            pass
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        else:
+            self.cache.remove(key)
+            self.cache.append(key)
+            return self.cache_data[key]
