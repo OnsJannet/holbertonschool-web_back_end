aeb3f5028407b63ee2d601a346409ebfad2f393f
onsjannet <onsjannet@gmail.com>
2021-07-28 18:00:23 +0100

    Hypermedia pagination

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -61,10 +61,11 @@ class Server:
         '''
         data = self.get_page(page, page_size)
         total_pages = math.floor(len(self.dataset()) / page_size)
+        page_sizes = len(self.get_page(page, page_size))
         next_page = page + 1 if page + 1 < total_pages else None
         prev_page = page - 1 if page - 1 < total_pages else None
         assert type(page) == int and page > 0
         assert type(page_size) == int and page_size > 0
-        return {'page_size': page_size, 'page': page, 'data': data,
+        return {'page_size': page_sizes, 'page': page, 'data': data,
                 'next_page': next_page, 'prev_page': prev_page,
                 'total_pages': total_pages}
