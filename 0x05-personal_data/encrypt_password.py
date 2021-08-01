577ce8e429e19f55abb1fc269f9b5ab2397d6542
onsjannet <onsjannet@gmail.com>
2021-08-01 18:03:04 +0100

    Check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -16,7 +16,7 @@ def hash_password(password: str) -> bytes:
     return hashed
 
 
-def is_valid(hashed_password: bytes, password: str):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     '''
     returns a boolean.
     '''
