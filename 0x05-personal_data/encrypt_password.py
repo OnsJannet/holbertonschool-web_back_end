57753b6e49539225df31b765cf05ed23297962bc
onsjannet <onsjannet@gmail.com>
2021-08-01 18:01:34 +0100

    Check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -14,3 +14,13 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str):
+    '''
+    returns a boolean.
+    '''
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
