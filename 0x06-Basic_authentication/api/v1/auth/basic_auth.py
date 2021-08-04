215bf3ef6e32af01abc4a624c73b4401782223d1
onsjannet <onsjannet@gmail.com>
2021-08-04 22:14:49 +0100

    Basic - User credentials

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,3 +45,20 @@ class BasicAuth(Auth):
             return header_encode
         except BaseException:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+
+        '''
+         the user email and password from the Base64 decoded value.
+        '''
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if not (":") in decoded_base64_authorization_header:
+            return None, None
+        else:
+            return tuple(decoded_base64_authorization_header.split(':', 1))
+
