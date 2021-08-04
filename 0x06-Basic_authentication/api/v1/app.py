80d8765c20351854d73cdec65621fe5b8fdc0c41
onsjannet <onsjannet@gmail.com>
2021-08-04 19:20:45 +0100

    Error Handler: 403 forbidden

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -27,6 +27,12 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+@app.errorhandler(403)
+def not_found(error) -> str:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,13 @@ def unauthorized() -> str:
       - raises a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 401 error by using abort
+    """
+    abort(403)
+
