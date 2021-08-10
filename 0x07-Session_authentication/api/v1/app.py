f4cf45d6f4625093359fd83887baf348ce1ad632
onsjannet <onsjannet@gmail.com>
2021-08-10 18:54:42 +0100

    created session auth class

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -21,6 +21,9 @@ if getenv('AUTH_TYPE') == 'auth':
 elif getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+session authentication
+'''
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    ''' enherits from Auth
+    '''
+    pass
