af27b5a814940019abce2cca649ed7d04727eafd
onsjannet <onsjannet@gmail.com>
2021-08-10 19:07:35 +0100

    added a function that creats an id

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,20 @@
 session authentication
 '''
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     ''' enherits from Auth
     '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates a Session ID for a user_id
+        '''
+        if user_id is None and type(user_id) is not str:
+            return None
+
+        ID = str(uuid4())
+        self.user_id_by_session_id[ID] = user_id
+        return ID
