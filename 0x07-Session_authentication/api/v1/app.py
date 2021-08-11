e8c90f6f7625ebfcb499333423d0b347a256f724
onsjannet <onsjannet@gmail.com>
2021-08-11 16:35:00 +0100

    Before request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -61,12 +61,11 @@ def before_request() -> None:
                              '/api/v1/forbidden/',
                              '/api/v1/auth_session/login/'
                              ]):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None \
+               and auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
-            if auth.session_cookie(request) is None:
-                abort(401)
 
     request.current_user = auth.current_user(request)
 
