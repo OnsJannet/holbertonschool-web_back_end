fc137a5d2673289f35eec66f91b9037accaa8fb5
onsjannet <onsjannet@gmail.com>
2021-08-15 18:47:13 +0100

    added register_user: Register users

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -5,7 +5,9 @@ AUTH
 
 import bcrypt
 from db import DB
-from user import User
+from user import Base, User
+
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
