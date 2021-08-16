e40600ace0c65af6124379c47b32b720b7181a60
onsjannet <onsjannet@gmail.com>
2021-08-16 17:16:20 +0100

    correction

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -7,7 +7,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-from user import Base, User
+
+from user import Base
+from user import User
 
 
 class DB:
