e9d9c1869acffb96beeae85dcb8de3ad9f25f1d9
onsjannet <onsjannet@gmail.com>
2021-08-17 18:12:59 +0100

    correction

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,12 +4,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-
-from user import Base
-from user import User
+from user import Base, User
 
 
 class DB:
@@ -25,7 +22,7 @@ class DB:
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def _session(self):
         """Memoized session object
         """
         if self.__session is None:
@@ -75,5 +72,4 @@ class DB:
 
         for key, value in kwargs.items():
             setattr(user, key, value)
-
         self._session.commit()
