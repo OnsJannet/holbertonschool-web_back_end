ab1d5b19fc2e8cf9d75908dcdcada0cc1e22e36f
onsjannet <onsjannet@gmail.com>
2021-08-18 17:30:53 +0100

    correction: app.py

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -44,7 +44,7 @@ def logout():
     ''' logout '''
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user is False:
+    if not user:
         abort(403)
 
     AUTH.destroy_session(user.id)
