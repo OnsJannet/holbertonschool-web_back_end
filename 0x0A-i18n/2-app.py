074e96cbc6c720cb0da6202093d78b2f6ea914fc
onsjannet <onsjannet@gmail.com>
2021-08-26 16:19:15 +0100

    get_locale

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -28,6 +28,7 @@ def index():
 
 @babel.localeselector
 def get_locale():
+    '''determine the best match with our supported languages.'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
