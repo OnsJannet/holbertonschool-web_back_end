27097d83adcffa3d0fc74a03e0de07d5d092e8a8
onsjannet <onsjannet@gmail.com>
2021-08-26 16:02:41 +0100

    correction

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -9,10 +9,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-'''0-index.html.'''
+    '''0-index.html.'''
     return render_template("0-index.html")
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-
