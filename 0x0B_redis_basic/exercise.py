a85e893d299d1e35a51e4763bceb367c96797437
onsjannet <onsjannet@gmail.com>
2021-08-31 16:36:58 +0100

    correction

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -9,7 +9,7 @@ from typing import Union, Callable, Optional
 from functools import wraps
 
 
-def count_calls(methode: Callable) -> Callable:
+def count_calls(method: Callable) -> Callable:
     ''' counts the times a function is 
         being called
     '''
@@ -18,8 +18,42 @@ def count_calls(methode: Callable) -> Callable:
     def wrapper(self, *args, **kwargs):
         ''' wrapper '''
         self._redis.incr(key)
-        return method(self, rgs, **kwargs)
+        return method(self, *args, **kwargs)
     return wrapper
+
+def call_history(method: Callable) -> Callable:
+    ''' store the history of inputs
+        and outputs for a particular function.
+    '''
+    call_key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        ''' wrapper '''
+        str_input = str(args)
+        self._redis.rpush(call_key  + ':inputs', str_input)
+        self._redis.rpush(
+            call_key  + ':outputs',
+            method(self, *args, **kwargs)
+        )
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def replay(fn: Callable) -> str:
+    ''' display the history of 
+        calls of a particular function. 
+    '''
+    data = fn.__qualname__
+    inputs = f"{data}:inputs"
+    outputs = f"{data}:outputs"
+    redis_ = fn.__self__._redis
+    input_redis = redis_.lrange(inputs, 0, -1)
+    count = redis_.lrange(outputs, 0, -1)
+    Q = fn.__self__._redis.get(data).decode('utf-8')
+    print(f"{data} was called {Q} times:")
+    for i, j in zip(input_redis, count):
+        print(f"{data}(*{i.decode('utf-8')}) -> {j.decode('utf-8')}")
 
 
 class Cache:
@@ -29,6 +63,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' Stores a key '''
         key = str(uuid.uuid4())
