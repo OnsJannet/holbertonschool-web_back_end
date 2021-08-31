386b0d282f049a90958977f780ed73597ece41e4
onsjannet <onsjannet@gmail.com>
2021-08-31 15:44:56 +0100

    Incrementing valuesé

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,7 +5,7 @@ exercise
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -20,3 +20,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, data: str, fn: Optional[Callable]) -> \
+            Union[str, bytes, int, float]:
+        ''' returns stored data '''
+        fn_data = self._redis.get(data)
+        if data:
+            return fn(fn_data)
+        else:
+            return fn_data
+
+    def get_str() -> str:
+        ''' parametrize Cache.get with the correct
+            conversion function
+        '''
+        return data.decode('utf-8')
+
+    def get_int() -> int:
+        ''' parametrize Cache.get with the correct
+            conversion function
+        '''
+        return int(data)
