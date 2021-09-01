3409fd57fdb67017429b92463e246ec1beedf858
onsjannet <onsjannet@gmail.com>
2021-09-01 14:03:33 +0100

    table creation

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -4,8 +4,7 @@
 --  name, string (255 characters)
 
 CREATE TABLE IF NOT EXISTS `users` (
-`id` INT NOT NULL UNIQUE AUTO_INCREMENT,
-`email` VARCHAR(255) NOT NULL,
-`name` VARCHAR(255)
-PRIMARY KEY  (`id`)
+`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`email` VARCHAR(255) NOT NULL UNIQUE,
+`name` varchar(255)
 );
