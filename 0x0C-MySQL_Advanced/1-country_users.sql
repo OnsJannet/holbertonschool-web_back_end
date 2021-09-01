92b0fff5ea976717bb600e85caaca43cf513d64e
onsjannet <onsjannet@gmail.com>
2021-09-01 15:20:13 +0100

    enum default

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,11 @@
+-- creates a tables users with certain requirements
+-- id, integer, never null, auto increment and primary key
+--  email, string (255 characters), never null and unique
+--  name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`email` VARCHAR(255) NOT NULL UNIQUE,
+`name` varchar(255)
+`country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
