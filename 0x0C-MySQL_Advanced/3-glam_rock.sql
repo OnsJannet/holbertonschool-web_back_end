1e45fb9177386124b6dd5be6144a97d241da6e69
onsjannet <onsjannet@gmail.com>
2021-09-02 18:27:31 +0100

    correction

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- Ranks country of origins by the number of non-unique fans
+--life span is diff between split and formed
 SELECT band_name,
 		TIMESTAMPDIFF(YEAR, `formed`, `split`) AS lifespan FROM metal_bands
 FROM metal_bands
