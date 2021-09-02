8eec49218ee275d648f5ed4ca3ab88a5ec5fe079
onsjannet <onsjannet@gmail.com>
2021-09-02 18:22:25 +0100

    correction

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Ranks country of origins by the number of non-unique fans
 SELECT `band_name`,
-	ifnull(split, YEAR(CURDATE())) - formed as lifespan
+		COALESCE(split, 2020) - formed as lifespan FROM metal_bands
 FROM `metal_bands`
 WHERE style LIKE '%Glam rock%';
 ORDER BY `lifespan` DESC;
