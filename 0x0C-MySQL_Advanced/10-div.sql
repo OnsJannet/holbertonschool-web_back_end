e067c7edbab37690e0498d0c0b9395bfd3ce4414
onsjannet <onsjannet@gmail.com>
2021-09-02 18:54:00 +0100

    uploaded files

---
+++ 0x0C-MySQL_Advanced/10-div.sql
@@ -0,0 +1,17 @@
+-- creates a function SafeDiv that divides (and returns)
+-- the first by the second number or returns 0 if the 
+-- second number is equal to 0.
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+BEGIN
+  DECLARE result FLOAT;
+  IF b = 0 THEN
+    SET result = 0;
+  ELSE
+    SET result = a / b;
+  END IF;
+  RETURN result;
+END$$
+DELIMITER ;
---
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- creates a trigger that decreases
+-- the quantity of an item after adding a new order.
+-- Quantity in the table items can be negative.
+CREATE TRIGGER decreasing_quantity
+AFTER INSERT ON orders
+FOR EACH ROW UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
