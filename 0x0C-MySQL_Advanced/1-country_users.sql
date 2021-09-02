ed2f3855938d9529613da77a979cf6ee92b5848c
onsjannet <onsjannet@gmail.com>
2021-09-02 07:22:39 +0100

    correction

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS `users` (
 `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 `email` VARCHAR(255) NOT NULL UNIQUE,
 `name` varchar(255)
-`country` ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+`country` ENUM('US', 'CO', 'TN') NOT NULL
 );
