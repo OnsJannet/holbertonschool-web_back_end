c9d493607ca2ead7b98c7e5a3af0f5b40c49196f
onsjannet <onsjannet@gmail.com>
2021-09-06 14:34:30 +0100

    [MongoDB x Python: Insert a document]

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""  inserts a new document in a 
+collection based on kwargs """
+
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ insert a new documents in a collection """
+    collection = mongo_collection
+    inserted = collection.insertOne(kwargs)
+    return (inserted.inserted_id)
