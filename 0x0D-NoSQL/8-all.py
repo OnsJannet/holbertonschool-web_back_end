4e7620c97d957d07d99fb72a483b52d0cfd459f9
onsjannet <onsjannet@gmail.com>
2021-09-06 14:21:36 +0100

    [MongoDB x Python: list all docs]

---
+++ 0x0D-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" List all documents in Python """
+
+from pymongo import MongoClient
+def list_all(mongo_collection):
+	""" lists all documents in a collection """
+	cursor = mongo_collection.mycollection
+	for document in cursor.find():
+		if document is None:
+			return ([])
+		else:
+			return (document)
