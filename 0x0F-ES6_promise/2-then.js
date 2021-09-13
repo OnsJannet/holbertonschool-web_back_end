1aa39d58d8faafbb52bd29a1431375285c9c85f4
onsjannet <onsjannet@gmail.com>
2021-09-13 19:51:50 +0100

    [ES6: catch]

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
+export default handleResponseFromAPI;
