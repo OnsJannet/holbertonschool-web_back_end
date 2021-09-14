69467390ec03de27c2b3ed98db20addd0c78172e
onsjannet <onsjannet@gmail.com>
2021-09-14 19:17:04 +0100

    [ES6: multiple promises]

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,9 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const user = await signUpUser(firstName, lastName);
- let photo = null;
- try {
-   photo = await uploadPhoto(fileName);
+  let photo = null;
+  try {
+    photo = await uploadPhoto(fileName);
   } catch (error) {
     photo = `${error.name}: ${error.message}`;
   }
