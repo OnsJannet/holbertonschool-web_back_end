8c2aa7e6b166819686e3e1adbb3c69e63070acaf
onsjannet <onsjannet@gmail.com>
2021-09-14 07:50:19 +0100

    [ES6: Promise]

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,7 +1,6 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) resolve({ status: 200, body: 'Success' });
     reject(Error('The fake API is not working currently'));
   });
 }
-export default getFullResponseFromAPI;
