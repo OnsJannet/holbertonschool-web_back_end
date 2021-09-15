fd935bb9cf0e23ee6d9ee8011ee4d311715dcdbc
onsjannet <onsjannet@gmail.com>
2021-09-15 13:41:43 +0100

    [ES6: setter and getter]

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,38 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (Array.isArray(students) !== true) throw TypeError('Students must be an array');
+    for (const student of students) if (typeof student !== 'string') throw TypeError('Students must be strings');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    this._length = length;
+  }
+
+  set students(students) {
+    if (Array.isArray(students) !== true) throw TypeError('Students must be an array');
+    this._students = students;
+  }
+}
