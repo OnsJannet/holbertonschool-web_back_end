c544c57afc44134dd1439b9ce758c29a0220ab6c
onsjannet <onsjannet@gmail.com>
2021-09-20 20:03:52 +0100

    [ES6: combine filter and map]

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(StudentList, city, newGrades) {
+  return StudentList
+    .filter((student) => student.location === city)
+    .map((student) => Object.assign(student,
+      {
+        grade: newGrades.filter((grade) => student.id === grade.studentId)[0]
+          ? newGrades.filter((grade) => student.id === grade.studentId)[0].grade : 'N/A',
+      }));
+}
