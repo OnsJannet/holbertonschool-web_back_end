97a9682faedcaba0c143bb37cebe2c662694d2ce
onsjannet <onsjannet@gmail.com>
2021-09-27 13:30:12 +0100

    [NodeJS: Using Process stdin]

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdin.resume();
 
-process.stdout.write('Welcome to Holberton School, what is your name?\r\n');
+console.log('Welcome to Holberton School, what is your name?\r\n');
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
