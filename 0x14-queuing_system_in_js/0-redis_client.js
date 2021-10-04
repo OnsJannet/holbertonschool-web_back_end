8b27b0b876aa89e9901dd13c8b3a1a33bd852b6b
onsjannet <onsjannet@gmail.com>
2021-10-04 08:14:01 +0100

    [JS Quering System: Node Redis Client]

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+import redis from "redis";
+const client = redis.createClient();
+
+client.on("connect", function() {
+  console.log("Redis client connected to the server");
+});
+
+client.on("error", function(error) {
+  console.log(`Redis client not connected to the server: ${error.toString()}`);
+});
