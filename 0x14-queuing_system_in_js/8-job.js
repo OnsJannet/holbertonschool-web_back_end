bd32b003dec2d11ab6235d581adfb972db8ee4db
onsjannet <onsjannet@gmail.com>
2021-10-05 09:05:11 +0100

    [JS: Kue]

---
+++ 0x14-queuing_system_in_js/8-job.js
@@ -0,0 +1,17 @@
+export default function createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.isArray(jobs)) throw Error("Jobs is not an array");
+  jobs.forEach((jb) => {
+    const job = queue.create("push_notification_code_3", jb).save((err) => {
+      if (!err) console.log(`Notification job created: ${job.id}`);
+    });
+    job.on("complete", () =>
+      console.log(`Notification job ${job.id} completed`)
+    );
+    job.on("failed", (error) =>
+      console.log(`Notification job ${job.id} failed: ${error}`)
+    );
+    job.on("progress", (progress) =>
+      console.log(`Notification job ${job.id} ${progress}% complete`)
+    );
+  });
+};
